ee8b9a20e3436edb8779ae8ef11f056d2eb64d1c
devofficer <myeyesontech@gmail.com>
2022-12-14 08:56:36 -0800

    [fixed] remove email address

--- src/pages/landing/landing.tsx
+++ src/pages/landing/landing.tsx
@@ -6,10 +6,9 @@ export const Landing = () => {
         src="assets/images/landing/bg.png"
         className="w-full -z-10"
       />
-      <div className="top-0 pt-64 px-[265px] space-y-12 absolute w-full justify-center">
-        <h1 className="text-4xl font-bold text-center">
-          alexontech1212@gmail.com<br/>
-          Can you please send email here?
+      <div className="top-0 pt-64 px-[265px] space-y-12 absolute w-full">
+        <h1 className="text-8xl font-bold uppercase text-center">
+          Welcome to Meishu platfrom.
         </h1>
         <div className="flex justify-center space-x-8">
           <div className="button-md primary">Stake my NFT</div>
