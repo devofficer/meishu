405743974f3350675951c05303b75474710ebd6b
devofficer <myeyesontech@gmail.com>
2022-12-14 08:49:16 -0800

    [feat] email

--- src/pages/landing/landing.tsx
+++ src/pages/landing/landing.tsx
@@ -6,9 +6,10 @@ export const Landing = () => {
         src="assets/images/landing/bg.png"
         className="w-full -z-10"
       />
-      <div className="top-0 pt-64 px-[265px] space-y-12 absolute">
-        <h1 className="text-8xl font-bold uppercase text-center">
-          Welcome to Meishu platfrom.
+      <div className="top-0 pt-64 px-[265px] space-y-12 absolute w-full justify-center">
+        <h1 className="text-4xl font-bold text-center">
+          alexontech1212@gmail.com<br/>
+          Can you please send email here?
         </h1>
         <div className="flex justify-center space-x-8">
           <div className="button-md primary">Stake my NFT</div>
